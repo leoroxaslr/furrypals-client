7f31358fdf088c842fa8413434ba22a20037dbf0
leoroxaslr <leoroxas.lr@gmail.com>
2023-09-21 17:28:52 +0800

    registration page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import LoginPic from "../assets/Veterinary-amico.svg";
 
 const Login = () => {
   return (
     <>
-      <section class="bg-white m-16">
-        <div class="grid max-w-screen-2xl px-5 py-8 mx-auto lg:py-16 lg:grid-cols-12">
-          <div class="hidden place-self-center w-96 lg:mt-0 lg:col-span-7 lg:flex">
-            <img class="scale-150" src={LoginPic} alt="face" />
+      <section className="bg-white m-16">
+        <div className="grid max-w-screen-2xl px-5 py-8 mx-auto lg:py-16 lg:grid-cols-12">
+          <div className="hidden place-self-center w-96 lg:mt-0 lg:col-span-7 lg:flex">
+            <img className="scale-150" src={LoginPic} alt="face" />
           </div>
-          <div class="m-auto place-self-center lg:col-span-5 lg:flex">
-            <div class="bg-white rounded-lg shadow drop-shadow-2xl md:mt-0 sm:max-w-md xl:p-0 w-auto">
-              <div class="p-10 space-y-4 md:space-y-6 sm:p-8">
-                <h1 class="text-3xl mb-10 font-bold leading-tight tracking-tight text-blue-950 md:text-3xl">
+          <div className="m-auto place-self-center lg:col-span-5 lg:flex">
+            <div className="bg-white rounded-lg shadow drop-shadow-2xl md:mt-0 sm:max-w-md xl:p-0 w-auto">
+              <div className="p-10 space-y-4 md:space-y-6 sm:p-8">
+                <h1 className="text-3xl mb-10 font-bold leading-tight tracking-tight text-blue-950 md:text-3xl">
                   Login
                 </h1>
-                <form class="space-y-4 md:space-y-6" action="#">
+                <form className="space-y-4 md:space-y-6" action="#">
                   <div>
                     <label
-                      for="email"
-                      class="block mb-2 text-sm font-medium text-gray-800"
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-800"
                     >
                       Your email
                     </label>
@@ -27,15 +28,15 @@ const Login = () => {
                       type="email"
                       name="email"
                       id="email"
-                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-blue-950 block w-full p-2"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-blue-950 block w-full p-2"
                       placeholder="name@email.com"
                       required=""
                     />
                   </div>
                   <div>
                     <label
-                      for="password"
-                      class="block mb-2 text-sm font-medium text-gray-800"
+                      htmlFor="password"
+                      className="block mb-2 text-sm font-medium text-gray-800"
                     >
                       Password
                     </label>
@@ -44,47 +45,48 @@ const Login = () => {
                       name="password"
                       id="password"
                       placeholder="••••••••"
-                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-950 focus:border-primary-600 block w-full p-2"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-950 focus:border-primary-600 block w-full p-2"
                       required=""
                     />
                   </div>
-                  <div class="flex items-start">
-                    <div class="flex items-center h-5">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
                       <input
                         id="remember"
                         aria-describedby="remember"
                         type="checkbox"
-                        class="w-4 h-4 border border-gray-800 rounded bg-slate-50 focus:ring-3 focus:ring-primary-300"
+                        className="w-4 h-4 border border-gray-800 rounded bg-slate-50 focus:ring-3 focus:ring-primary-300"
                         required=""
                       />
                     </div>
-                    <div class="ml-1 mr-16 text-sm">
-                      <label for="remember" class="text-gray-800">
+                    <div className="ml-1 mr-16 text-sm">
+                      <label htmlFor="remember" className="text-gray-800">
                         Remember me
                       </label>
                     </div>
-                    <a
+                    <Link
                       href="#"
-                      class="text-sm font-medium text-gray-800 hover:underline ml-10"
+                      className="text-sm font-medium text-gray-800 hover:underline ml-10"
                     >
                       Forgot password?
-                    </a>
+                    </Link>
                   </div>
 
                   <button
                     type="submit"
-                    class="w-full text-white bg-purple-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    className="w-full text-white bg-purple-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
                   >
                     Sign in
                   </button>
-                  <p class="text-sm font-light text-gray-800">
+                  <p className="text-sm font-light text-gray-800">
                     Don’t have an account yet?
-                    <a
-                      href="./signup.html"
-                      class="font-medium text-gray-800 hover:underline"
+                    <Link
+                      as={Link}
+                      to="/apply"
+                      className="font-medium text-gray-800 hover:underline"
                     >
                       Sign up
-                    </a>
+                    </Link>
                   </p>
                 </form>
               </div>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,117 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import LoginPic from "../assets/Veterinary-amico.svg";
 
 const SignUp = () => {
   return (
     <>
-      <div className="h-80">
-        <h1>Signup</h1>
-      </div>
+      <section className="bg-white m-16">
+        <div className="grid max-w-screen-2xl px-5 py-8 mx-auto lg:py-16 lg:grid-cols-12">
+          <div className="hidden place-self-center w-96 lg:mt-0 lg:col-span-7 lg:flex">
+            <img className="scale-150" src={LoginPic} alt="face" />
+          </div>
+          <div className="m-auto place-self-center lg:col-span-5 lg:flex">
+            <div className="bg-white rounded-lg shadow drop-shadow-2xl md:mt-0 sm:max-w-md xl:p-0 w-auto">
+              <div className="p-10 space-y-4 md:space-y-6 sm:p-8">
+                <h1 className="text-3xl font-bold leading-tight tracking-tight text-blue-950 md:text-3xl">
+                  Create an account
+                </h1>
+                <form className="space-y-4 md:space-y-6" action="#">
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2"
+                      placeholder="name@email.com"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="confirm-password"
+                      className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                      Confirm password
+                    </label>
+                    <input
+                      type="confirm-password"
+                      name="confirm-password"
+                      id="confirm-password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2"
+                      required=""
+                    />
+                  </div>
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="terms"
+                        aria-describedby="terms"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-offset-amber-400 focus:ring-yellow-300"
+                        required=""
+                      />
+                    </div>
+                    <div className="ml-3 lg:mr-28 text-sm">
+                      <label
+                        htmlFor="terms"
+                        className="font-light text-gray-800"
+                      >
+                        I accept the
+                        <Link
+                          className="font-medium text-gray-800 hover:underline lg:mr-5 mr-10"
+                          href="#"
+                        >
+                          Terms and Conditions
+                        </Link>
+                      </label>
+                    </div>
+                  </div>
+                  <button
+                    type="submit"
+                    className="w-full text-white bg-purple-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                  >
+                    Create an account
+                  </button>
+                  <p className="text-sm font-light text-gray-800">
+                    Already have an account?
+                    <Link
+                      as={Link}
+                      to="/signin"
+                      className="font-medium text-gray-800 hover:underline"
+                    >
+                      Login here
+                    </Link>
+                  </p>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };

1cc1474fddb9509a4348c5e026bdd054a8c44e43
leoroxaslr <leoroxas.lr@gmail.com>
2023-09-24 02:54:55 +0800

    init firebase and registration

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -25,6 +25,8 @@ const SignUp = () => {
   const [errMsg, setErrMsg] = useState("");
   const [success, setSuccess] = useState(false);
 
+  const [isChecked, setIsChecked] = useState(false);
+
   useEffect(() => {
     userRef.current.focus();
   }, []);
@@ -49,6 +51,22 @@ const SignUp = () => {
     setErrMsg("");
   }, [user, pwd, matchPwd]);
 
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const v1 = USER_REGEX.test(user);
+    const v2 = PWD_REGEX.test(pwd);
+    if (!v1 || v2) {
+      setErrMsg("Invalid Entry");
+      return;
+    }
+    console.log(user, pwd);
+    setSuccess(true);
+  };
+
   return (
     <>
       <section className="bg-white m-16">
@@ -69,14 +87,18 @@ const SignUp = () => {
                 >
                   {errMsg}
                 </p>
-                <form className="space-y-4 md:space-y-6" action="#">
-                  <div>
+                <form
+                  className="space-y-4 md:space-y-6"
+                  action="#"
+                  onSubmit={handleSubmit}
+                >
+                  <div className="uname">
                     <label
                       htmlFor="username"
                       className="text-sm font-medium text-gray-900 flex justify-between "
                     >
                       Your username
-                      <span className={validName ? "vali d" : "hide"}>
+                      <span className={validName ? "valid" : "hide"}>
                         <svg
                           className="w-[14px] h-[14px] text-gray-800 dark:text-white "
                           aria-hidden="true"
@@ -103,7 +125,6 @@ const SignUp = () => {
                     <input
                       type="text"
                       id="username"
-                      name="username"
                       ref={userRef}
                       autoComplete="off"
                       onChange={(e) => setUser(e.target.value)}
@@ -118,52 +139,136 @@ const SignUp = () => {
                     <p
                       id="uidnote"
                       className={
-                        userFocus && user && !validName
-                          ? "instructions"
-                          : "offscreen"
+                        userFocus && !validName ? "instructions" : "offscreen"
                       }
                     >
+                      4-24 characters <br />
                       Must begin with a letter. <br />
-                      Only alphanumeric characters are allowed <br />
+                      Letters, numbers, underscores, hypens allowed.
                     </p>
                   </div>
-                  <div>
+
+                  <div className="passw">
                     <label
                       htmlFor="password"
-                      className="block mb-2 text-sm font-medium text-gray-900"
+                      className="text-sm font-medium text-gray-900 flex justify-between "
                     >
-                      Password
+                      Your Password
+                      <span className={validPwd ? "valid" : "hide"}>
+                        <svg
+                          className="w-[14px] h-[14px] text-gray-800 dark:text-white "
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="green"
+                          viewBox="0 0 20 20"
+                        >
+                          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                      </span>
+                      <span className={validPwd || !pwd ? "hide" : "invalid"}>
+                        <svg
+                          className="w-[14px] h-[14px] text-gray-800 dark:text-white"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="red"
+                          viewBox="0 0 20 20"
+                        >
+                          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
+                        </svg>
+                      </span>
                     </label>
                     <input
                       type="password"
-                      name="password"
                       id="password"
+                      autoComplete="off"
+                      onChange={(e) => setPwd(e.target.value)}
+                      required
+                      aria-invalid={validPwd ? "false" : "true"}
+                      aria-describedby="pwdnote"
+                      onFocus={() => setPwdFocus(true)}
+                      onBlur={() => setPwdFocus(false)}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2"
                       placeholder="••••••••"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2"
-                      required=""
                     />
-                  </div>
-                  <div>
-                    <label
-                      htmlFor="confirm-password"
-                      className="block mb-2 text-sm font-medium text-gray-900"
+                    <p
+                      id="pwdnote"
+                      className={
+                        pwdFocus && !validPwd ? "instructions" : "offscreen"
+                      }
                     >
-                      Confirm password
+                      8-24 characters
+                      <br />
+                      Must include uppercase and lowercase <br /> letters,a
+                      number and a special character. <br />
+                      Allowed special character:
+                      <span aria-label="exclamation mark">!</span>
+                      <span aria-label="at symbol">@</span>
+                      <span aria-label="hastag">#</span>
+                      <span aria-label="Dollar Sign">$</span>
+                    </p>
+                  </div>
+                  <div className="confirmpass">
+                    <label
+                      htmlFor="confirm_pwd"
+                      className="text-sm font-medium text-gray-900 flex justify-between "
+                    >
+                      Confirm Password
+                      <span
+                        className={validMatch && matchPwd ? "valid" : "hide"}
+                      >
+                        <svg
+                          className="w-[14px] h-[14px] text-gray-800 dark:text-white "
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="green"
+                          viewBox="0 0 20 20"
+                        >
+                          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                        </svg>
+                      </span>
+                      <span
+                        className={validMatch || !matchPwd ? "hide" : "invalid"}
+                      >
+                        <svg
+                          className="w-[14px] h-[14px] text-gray-800 dark:text-white"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="red"
+                          viewBox="0 0 20 20"
+                        >
+                          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
+                        </svg>
+                      </span>
                     </label>
                     <input
-                      type="confirm-password"
-                      name="confirm-password"
-                      id="confirm-password"
+                      type="password"
+                      id="confirm_pwd"
+                      autoComplete="off"
+                      onChange={(e) => setMatchPwd(e.target.value)}
+                      required
+                      aria-invalid={validMatch ? "false" : "true"}
+                      aria-describedby="confnote"
+                      onFocus={() => setMatchFocus(true)}
+                      onBlur={() => setMatchFocus(false)}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2"
                       placeholder="••••••••"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2"
-                      required=""
                     />
+                    <p
+                      id="confnote"
+                      className={
+                        matchFocus && !validMatch ? "instructions" : "offscreen"
+                      }
+                    >
+                      Must match the first password input field.
+                    </p>
                   </div>
                   <div className="flex items-start">
                     <div className="flex items-center h-5">
                       <input
                         id="terms"
                         aria-describedby="terms"
+                        checked={isChecked}
+                        onChange={handleCheckboxChange}
                         type="checkbox"
                         className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-offset-amber-400 focus:ring-yellow-300"
                         required=""
@@ -185,6 +290,11 @@ const SignUp = () => {
                     </div>
                   </div>
                   <button
+                    disabled={
+                      !isChecked || !validName || !validPwd || !validMatch
+                        ? true
+                        : false
+                    }
                     type="submit"
                     className="w-full text-white bg-purple-400 hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                   >
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAAUeIYf7JkfR5JfMWvAZoj-DUgn0-EZW8",
+  authDomain: "furrypals-d96a5.firebaseapp.com",
+  projectId: "furrypals-d96a5",
+  storageBucket: "furrypals-d96a5.appspot.com",
+  messagingSenderId: "481223081258",
+  appId: "1:481223081258:web:6ddd083c3b9ad83de9f253",
+  measurementId: "G-76S5EKY757",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

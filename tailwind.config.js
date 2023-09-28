bb7f945246f3c487b7d15bd43cb68180c0ade0cc
leoroxaslr <leoroxas.lr@gmail.com>
2023-09-29 01:58:31 +0800

    updated theme 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   daisyui: {
     themes: ["cupcake"],
-  },
-  theme: {
-    extend: {},
   },
   plugins: [require("daisyui")],
 };

61dde31f368d526d5e1935785f44a7d3c64abeef
leoroxaslr <leoroxas.lr@gmail.com>
2023-09-29 01:58:26 +0800

    updated theme 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  fontFamily: {
-    display: ["Mulish"],
-  },
   daisyui: {
     themes: ["cupcake"],
   },

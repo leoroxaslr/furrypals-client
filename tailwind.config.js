ed67fa7cdbba78ad7f73a62b67f152f8e1e1fccf
leoroxaslr <leoroxas.lr@gmail.com>
2023-09-29 01:39:42 +0800

    updated theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,29 +5,7 @@ export default {
     display: ["Mulish"],
   },
   daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#65c3c8",
-
-          secondary: "#ef9fbc",
-
-          accent: "#eeaf3a",
-
-          neutral: "#374151",
-
-          "base-100": "#f5f5f4",
-
-          info: "#3abff8",
-
-          success: "#36d399",
-
-          warning: "#fbbd23",
-
-          error: "#f87272",
-        },
-      },
-    ],
+    themes: ["cupcake"],
   },
   plugins: [require("daisyui")],
 };

bcec4e8b2de2e2d3b3d7e1cca76136957e788119
leoroxaslr <leoroxas.lr@gmail.com>
2023-09-29 06:01:58 +0800

    fixed bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   daisyui: {
     themes: ["cupcake"],
   },
